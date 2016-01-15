0f7859cc5e9534272dd6e0a692abfc3592cbcf77
Cheukyin Yip <cheukyinyip0077@gmail.com>
2016-01-15 16:34:46 +0800

    init ppm_writer.hpp

---
+++ ppm_writer.hpp
@@ -0,0 +1,11 @@
+#ifndef CHEUKYIN_RAYTRACER_PPM_WRITER_H
+#define CHEUKYIN_RAYTRACER_PPM_WRITER_H
+
+namespace RayTracer{
+    namespace ImageWriter
+    {
+    } // namespace ImageWriter
+} // namespace RayTracer
+
+
+#endif // CHEUKYIN_RAYTRACER_PPM_WRITER_H
